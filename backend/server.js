3ac8ce39880b7e4f71910e39f8404b2dd099a320
Maulik's MAC <macbook@192.168.0.107>
2026-05-07 18:11:56 +0530

    fix appointment backend urls

--- backend/server.js
+++ backend/server.js
@@ -175,11 +175,9 @@ async function getMeetLinkSafely(date, time) {
  * EMAIL (GMAIL SMTP)
  ******************************/
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
-  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
